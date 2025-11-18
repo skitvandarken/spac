816c8b25f241f5e88ac7a6cf9db90c78b8de3635
Pedro Jorge <skitvandarken2013@gmail.com>
2025-11-18 18:26:15 +0100

    botoes corrigidos

--- src/app/paginas/inicio/inicio.ts
+++ src/app/paginas/inicio/inicio.ts
@@ -3,13 +3,14 @@ import { Menu } from '../../layout/menu/menu';
 import { Hero } from '../../layout/hero/hero';
 import { Form } from '../../layout/form/form';
 import { Rodape } from '../../layout/rodape/rodape';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-inicio',
   imports: [
-
-    Menu, Hero, Form, Rodape, 
-  ],
+    Menu, Hero, Form, Rodape,
+    RouterLink
+],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
